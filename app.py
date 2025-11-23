54ddbd7cbf7866a03bb9d0c9ae00679b86885afc
IdrisFallout <dev@waithakasam.com>
2025-11-23 16:42:52 +0300

    ci(Argocd): Add Kubernetes manifest
    - link repo with argocd
    
    Signed-off-by: Samwel Waithaka <dev@waithakasam.com>

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Kubernetes Demo App1!'
 
+@app.route('/health')
+def health():
+    return {'status': 'healthy'}, 200
+
 
 if __name__ == '__main__':
     app.run()
